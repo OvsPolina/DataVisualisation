d13c52092462873e3c2157eaf21311103c4b733f
OvsPolina <58885435+OvsPolina@users.noreply.github.com>
2023-12-15 23:02:27 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const ctx = {
     SVG_W: 2000,
-    SVG_H: 3000,
+    SVG_H: 2000,
     barChartWidth: 100,
     barChartHeight: 50,
     barSpacing: 10,
@@ -12,21 +12,26 @@ const ctx = {
                 "Mortgage Interest Rate": 'x55', 
                 "Price per Square Meter": 'x52', 
                 "Gasoline": 'x33'},
+    CitiesInfo: '',
+    selectedOption: 'McMeal',
+    selectedCountry: 'France',
 };
 
-function createBarChart(container, data, selectedOption) {
+function createBarChart(container, data) {
     const barChart = d3.create('svg')
         .attr('width', 1000)
         .attr('height', 1000)
         .style("opacity", 1);
         //.attr('transform', 'translate(500, 50)');
 
-    let column = ctx.topfeatures[selectedOption];
+    let column = ctx.topfeatures[ctx.selectedOption];
 
     const chartGroup = barChart.append("g")
         .attr('transform', 'translate(50, 30)');
 
-    const sortedData = data.sort((a, b) => b[column] - a[column]).slice(0, 10);
+    const filteredCities = data.filter(city => city.country === ctx.selectedCountry);
+
+    const sortedData = filteredCities.sort((a, b) => b[column] - a[column]).slice(0, 10);
 
     const yScale = d3.scaleBand()
         .domain(sortedData.map(d => d.city))
@@ -78,7 +83,7 @@ function createBarChart(container, data, selectedOption) {
     return barChart;
 }
 
-function createTable(container, data, selectedOption) {
+function createTable(container, data) {
     const table = d3.create('table').attr('x', 250).attr('y', 50);
     const thead = table.append('thead');
     const tbody = table.append('tbody');
@@ -90,10 +95,11 @@ function createTable(container, data, selectedOption) {
         headerRow.append('th').text(headerText);
     });
 
-    let column = ctx.topfeatures[selectedOption];
-    console.log(column);
+    let column = ctx.topfeatures[ctx.selectedOption];
 
-    const sortedData = data.sort((a, b) => b[column] - a[column]).slice(0,10);
+    const filteredCities = data.filter(city => city.country === ctx.selectedCountry);
+
+    const sortedData = filteredCities.sort((a, b) => b[column] - a[column]).slice(0,10);
 
     console.log(sortedData);
 
@@ -111,7 +117,7 @@ function createTable(container, data, selectedOption) {
 }
 
 function CreateTop(main, data){
-    let Top = main.append("svg").attr("id", "top").attr("width", 2000).attr("height", 500);
+    let Top = main.append("svg").attr("id", "top").attr("width", 1000).attr("height", 1000);
     const topGroup = Top.append("g").attr("id", "topgroup").attr("transform", "translate(0, 0)");
 
     // Создание выпадающего списка
@@ -122,7 +128,7 @@ function CreateTop(main, data){
         .attr("width", 150)
         .attr("height", 30)
         .append("xhtml:select")
-        .attr("transform", "translate(0, 0)")
+        //.attr("transform", "translate(1000, 0)")
         .attr("style", "width: 100%;");
 
     // Добавление опций в выпадающий список
@@ -148,7 +154,7 @@ function CreateTop(main, data){
         .attr("id", "tableContainer");
 
     // Добавление кнопки для отображения таблицы
-    topGroup.append("foreignObject")
+    /*topGroup.append("foreignObject")
         .attr("x", 400)
         .attr("y", 10)
         .attr("width", 100)
@@ -160,7 +166,7 @@ function CreateTop(main, data){
             //tableContainer.style("display", "block");
             tableContainer.style("display", "block");
             barChartContainer.style("display", "block");
-        });
+        });*/
 
     const barChartContainer = topGroup.append("foreignObject")
         .attr("width", 1000)
@@ -206,25 +212,36 @@ function CreateTop(main, data){
     // Обработчик события для выпадающего списка
     select.on("change", function () {
         const selectedOption = d3.select(this).property("value");
+        ctx.selectedOption = selectedOption;
+        
         tableContainer.html("");
         barChartContainer.html("");
-        createTable(tableContainer.node(), data, selectedOption);
-        createBarChart(barChartContainer.node(), data, selectedOption);
-        tableContainer.style("display", "none");
-        barChartContainer.style("display", "none");
+        createTable(tableContainer.node(), data);
+        createBarChart(barChartContainer.node(), data);
+        tableContainer.style("display", "block");
+        barChartContainer.style("display", "block");
+
+        //tableContainer.style("display", "none");
+        //barChartContainer.style("display", "none");
     });
 }
 
-function CreateMap(main, geo){
+function CreateMap(geo, data, cities){
+    
+    let main = d3.select("#map")
 
-    let Map = main.append("svg").attr("id", "map").attr("width", 1000).attr("height", 1000);
+    let Map = main.append("svg").attr("id", "map").attr("width", ctx.SVG_W).attr("height", ctx.SVG_H);
 
     const projection = d3.geoIdentity().reflectY(true).fitSize([ctx.SVG_W, ctx.SVG_H], geo);
     const pathGenerator = d3.geoPath().projection(projection);
+
+    const zoom = d3.zoom()
+    .scaleExtent([1, 8]) // Установка диапазона масштабирования
+    .on("zoom", zoomed);
     
     // Create a group for the map features
     const mapGroup = Map.append("g")
-                        .attr("transform", "translate(50, -1000)")
+                        .attr("transform", "translate(0, -500)")
                         .attr("width", ctx.SVG_W)
                         .attr("height", ctx.SVG_H);
 
@@ -235,14 +252,53 @@ function CreateMap(main, geo){
       .enter()
       .append("path")
       .attr("d", pathGenerator)
-      .style("fill", "lightblue") // Set a fill color
-      .style("stroke", "gray");
+      .style("fill", "red") // Set a fill color
+      .style("background", 'black')
+      .style("stroke", "white");
+
+    mapGroup.call(zoom);
+    mapGroup.on("wheel.zoom", null); // Отключаем стандартное масштабирование браузера
+
+    mapGroup.selectAll("path")
+      .on("mouseover", function (event, d) {
+          d3.select(this)
+              .style("fill", "orange"); // Измените цвет подсветки по своему выбору
+      })
+      .on("mouseout", function (event, d) {
+          d3.select(this)
+              .style("fill", "lightblue"); // Возвращение оригинального цвета
+      })
+      .on("click", function (event, d) {
+        d3.select(this)
+            .style("fill", "green"); // Возвращение оригинального цвета
+        ctx.selectedCountry = d.properties.name;
+        updateCountryMap(d, cities);
+        updateInfo(d, data);
+      });
+
+    const zoomInButton = main.append("button")
+      .text("Zoom In")
+      .attr("transform", "translate(0, 0)")
+      .on("click", function() {
+          mapGroup.transition().call(zoom.scaleBy, 1.2);
+      });
+
+    const zoomOutButton = main.append("button")
+      .text("Zoom Out")
+      .attr("transform", "translate(0,0)")
+      .on("click", function() {
+          mapGroup.transition().call(zoom.scaleBy, 0.8);
+      });
+
+    function zoomed(event) {
+        mapGroup.attr("transform", event.transform);
+    }
 
 }
 
 function CreatePlot(main, data) {
     let Plot = main.append("svg").attr("id", "plot").attr("width", 1000).attr("height", 1000);
-    const plotGroup = Plot.append("g").attr("id", "plotgroup").attr("transform", "translate(0, 0)");
+    const plotGroup = Plot.append("g").attr("id", "plotgroup").attr("transform", "translate(500, 0)");
 
     // Создание выпадающих списков для выбора осей
     const xSelect = plotGroup
@@ -350,6 +406,10 @@ function createScatterPlot(container, data, xOption, yOption) {
         .style("text-anchor", "middle")
         .text(yOption);
 
+    const tooltip = d3.select("body").append("div")
+        .attr("class", "tooltip")
+        .style("opacity", 0);
+
     scatterPlot.selectAll('circle')
         .data(data)
         .enter().append('circle')
@@ -358,31 +418,166 @@ function createScatterPlot(container, data, xOption, yOption) {
         .attr('r', 3)
         .attr("transform", "translate(50, 0)")
         .attr('fill', 'lightblue')
-        .style("opacity", 0.7);
+        .style("opacity", 0.7)
+        .on("mouseover", function (event, d) {
+            // Показать всплывающую подсказку при наведении мыши
+            tooltip.transition()
+                .duration(200)
+                .style("opacity", 0.9);
+            tooltip.html(d.city)
+                .style("left", (event.pageX + 5) + "px")
+                .style("top", (event.pageY - 18) + "px");
+        })
+        .on("mouseout", function (d) {
+            // Скрыть всплывающую подсказку при уходе мыши
+            tooltip.transition()
+                .duration(500)
+                .style("opacity", 0);
+        });
+
 
     container.appendChild(scatterPlot.node());
     return scatterPlot;
 }
 
+function CreateCountryMap(map){
+    let main = d3.select("#country").style("display", "inline-block");
 
-function loadData(main) {
+    let CountryMap = main.append("svg").attr("id", "country-map").attr("width", ctx.SVG_W/2).attr("height", ctx.SVG_H/2);
+
+    const projection = d3.geoIdentity().reflectY(true).fitSize([ctx.SVG_W/2, ctx.SVG_H/2], map);
+    const pathGenerator = d3.geoPath().projection(projection);
+    
+    // Create a group for the map features
+    const cmapGroup = CountryMap.append("g")
+                        .attr("transform", "translate(0, 0)")
+                        .attr("id", "city-markers")
+                        .attr("width", ctx.SVG_W/2)
+                        .attr("height", ctx.SVG_H/2);
+
+    // Bind the GeoJSON data to the path elements and draw them
+    cmapGroup.selectAll("path")
+      .data(map.features)
+      .enter()
+      .append("path")
+      .attr("d", pathGenerator)
+      .style("fill", "red") // Set a fill color
+      .style("background", 'black')
+      .style("stroke", "white");
+
+    cmapGroup.selectAll("path")
+      .on("mouseover", function (event, d) {
+          d3.select(this)
+              .style("fill", "orange"); // Измените цвет подсветки по своему выбору
+      })
+      .on("mouseout", function (event, d) {
+          d3.select(this)
+              .style("fill", "lightblue"); // Возвращение оригинального цвета
+      });
+}
+
+
+function CreateInfo(data){
+    let main = d3.select("#info").style("float", "right");
+    let Info = main.append("svg").attr("id", "info").attr("width", ctx.SVG_W/2-50).attr("height", ctx.SVG_H/2-50)
+                    .attr("alignment-baseline", "middle");
+
+    CreateTop(Info, data);
+    CreatePlot(Info, data);
+
+}
+
+function updateCountryMap(selectedCountry, cities) {
+    const selectedCountryId = selectedCountry.properties.geoname_id;
+
+    console.log(selectedCountry.properties.name);
+
+    // Assuming your city data is named 'cityData' and is in GeoJSON format
+    const filteredCities = cities.features.filter(city => city.properties.cou_name_en === selectedCountry.properties.name);
+    const cityNames = filteredCities.map(city => city.properties.name);
+    ctx.CitiesInfo = cityNames;
+    //console.log(cityNames);
+    //console.log(ctx.CitiesInfo);
+
+    // Customize the display of filteredCities as needed
+    const countryMapElement = d3.select("#country-map");
+    countryMapElement.html("");
+    countryMapElement.append("h2").text(selectedCountry.properties.name);
+    console.log(countryMapElement.node());
+
+    // Remove existing city markers
+    countryMapElement.select("#city-markers").remove();
+
+    // Create a new group for city markers
+    const cityMarkers = countryMapElement.append("g").attr("id", "city-markers");
+
+    // Create a projection for the map
+    const projection = d3.geoIdentity().reflectY(true).fitSize([ctx.SVG_W / 2, ctx.SVG_H / 2], selectedCountry);
+    const pathGenerator = d3.geoPath().projection(projection);
+
+    // Draw the map for the selected country
+    /*const cmapGroup = countryMapElement.append("g")
+        .attr("transform", "translate(0, 0)")
+        .attr("width", ctx.SVG_W / 2)
+        .attr("height", ctx.SVG_H / 2);*/
+
+    const cmapGroup = cityMarkers.selectAll("path")
+        .data(filteredCities)  // Pass only the selected country to the data
+        .enter()
+        .append("path")
+        .attr("d", pathGenerator)
+        .style("fill", "red") // Set a fill color
+        .style("background", 'black')
+        .style("stroke", "white");
+
+    // Bind the GeoJSON data to the path elements and draw them
+    /*cmapGroup.selectAll("path")
+        .data(filteredCities.properties)  // Передаем только выбранную страну в данные
+        .enter()
+        .append("path")
+        .attr("d", pathGenerator)
+        .style("fill", "orange") // Set a fill color
+        .style("background", 'black')
+        .style("stroke", "white");*/
+
+}
+
+function updateInfo(selectedCountry, data){
+   // const filteredCities = data.filter(city => ctx.CitiesInfo.includes(city.city));
+    const tableContainer = d3.selectAll("#tableContainer");
+    const barChartContainer = d3.selectAll("#barChartContainer");
+
+    tableContainer.html("");
+    barChartContainer.html("");
+
+    createTable(tableContainer.node(), data);
+    createBarChart(barChartContainer.node(), data);
+        
+   // tableContainer.style("display", "none");
+   // barChartContainer.style("display", "none");
+
+}
+
+function loadData() {
     Promise.all([
         d3.json("data/custom.geo.json"),
-        d3.csv("data/cost-of-living.csv")
+        d3.csv("data/cost-of-living.csv"),
+        d3.json("data/world.geojson")
       ])
         .then(function(data) {
 
           //createViz();
-          CreateMap(main, data[0]);
-          CreateTop(main, data[1]);
-          CreatePlot(main, data[1]);
+          CreateMap(data[0], data[1], data[2]);
+          CreateCountryMap(data[2]);
+          CreateInfo(data[1]);
+
+          d3.selectAll("map").call(zoom);
 
         }).catch(function (error) { console.log(error) });
 };
 
 function createViz() {
     console.log("Using D3 v" + d3.version);
-    let main = d3.select("#main")
-    loadData(main);
+    loadData();
 };
 
